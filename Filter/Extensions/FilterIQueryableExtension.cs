63c6572cdef40b1b277ec277875181dfea216d3c
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2021-08-04 23:44:04 -0400

    Add filter extensions.

---
+++ Filter/Extensions/FilterIQueryableExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Sintaxis.Collections.Filter.Extensions
+{
+    public static class FilterIQueryableExtension
+    {
+        public static IQueryable<T> FilterWhenTrue<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate)
+        {
+            var filteredSource = source.Where(predicate);
+            if (filteredSource.Count() == source.Count()) { 
+                return source;
+            }
+
+            return source;
+        }
+    }
+}
--- Sintaxis.Test/Pagination/PaginationTests.cs
+++ Sintaxis.Test/Pagination/PaginationTests.cs
@@ -10,13 +10,34 @@ namespace Sintaxis.Test
         [Fact]
         public void ShouldReturnExceptionWhenSourceParameterIsNull()
         {
+            int page = 1;
+            int pageSize = 10;
             IQueryable<int> mockCollection = Enumerable.Range(1, 100).AsQueryable();
             mockCollection = null;
 
-            var exception = Assert.Throws<ArgumentException>(() => mockCollection.Paginate(1,10));
+            var exception = Assert.Throws<ArgumentException>(() => mockCollection.Paginate(page, pageSize));
 
             Assert.IsType<ArgumentException>(exception);
         }
-        
+
+        [Fact]
+        public void ShouldReturnFrom10To30WhenPageIs2AndTotalIs100()
+        {
+            int expectedPaginated = 10;
+            int expectedFirstPaginatedElement = 11;
+            int expectedLastPaginatedElement = 20;
+            int page = 1;
+            int pageSize = 10;
+            IQueryable<int> mockCollection = Enumerable.Range(1, 100).AsQueryable();
+
+            var paginatedResult = mockCollection.Paginate(page, pageSize);
+            var firstPaginatedResult = paginatedResult.First();
+            var lastPaginatedResult = paginatedResult.Last();
+
+            Assert.Equal(paginatedResult.Count(), expectedPaginated);
+            Assert.Equal(expectedFirstPaginatedElement, firstPaginatedResult);
+            Assert.Equal(expectedLastPaginatedElement, lastPaginatedResult);
+        }
+
     }
 }
